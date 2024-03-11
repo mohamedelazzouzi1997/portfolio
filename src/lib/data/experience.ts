81a4768dfababc1b97d8966814316618cf40c1a4
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2024-03-11 01:47:58 +0000

    add experiances

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -1,52 +1,72 @@
 import Assets from './assets';
 import { getSkills } from './skills';
 import { ContractType, type Experience } from '../types';
+import smart_influance from '../md/Smart-Influencer.md?raw';
+import it_lab from '../md/it-lab.md?raw';
+import leblokk from '../md/le-blokk.md?raw';
+import digitalpole from '../md/digitalepole.md?raw';
+
 
 export const items: Array<Experience> = [
 	{
-		slug: 'open-sourcer',
-		company: 'Self-employed',
-		description: 'Creating awesome tools for developers.',
-		contract: ContractType.SelfEmployed,
+		slug: 'Full-stack-web-developer-Digitalpol',
+		company: 'Digitalpole',
+		description: digitalpole,
+		contract: ContractType.FullTime,
 		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('ts', 'js'),
-		name: 'Open Source Developer',
+		location: 'On Site',
+		period: { from: new Date(2023, 6, 1), to: new Date() },
+		skills: getSkills('css', 'html', 'Laravel', "PHP", 'tailwindcss', 'Angular', 'PostgreSQL'),
+		name: 'Full stack web developer',
+		color: 'skyblue',
+		links: [],
+		logo: 'https://www.digitalpole.fr/wp-content/uploads/2023/10/logo1.webp',
+		shortDescription: 'I created web applications using Laravel and Angular within an Agile framework, while utilizing Git and Jira for efficient project management.'
+	},
+	{
+		slug: 'Full-stack-web-developer-le-blokk',
+		company: 'Conciergety / le blokk restaurent',
+		description: leblokk,
+		contract: ContractType.FullTime,
+		type: 'Software Development',
+		location: 'On Site',
+		period: { from: new Date(2023, 0, 1), to: new Date(2023, 6, 29) },
+		skills: getSkills('css', 'html', 'js', 'MySQL', 'Laravel', "PHP", 'tailwindcss'),
+		name: 'Full stack web developer',
 		color: 'red',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome tools for developers.'
+		shortDescription: 'I built two schedule apps with Laravel and FullCalendar.js.'
 	},
 	{
-		slug: 'software-freelance',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
+		slug: 'Full-stack-web-developer-it-labs',
+		company: 'IT LABS PRO',
+		description: it_lab,
+		contract: ContractType.Internship,
 		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
-		name: 'Freelancer',
+		location: 'On Site',
+		period: { from: new Date(2022, 5, 29), to: new Date(2022, 11, 30) },
+		skills: getSkills('css', 'html', 'js', 'MySQL', 'Laravel', "PHP"),
+		name: 'Full stack web developer',
 		color: 'blue',
 		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
+		logo: 'https://www.itlabspro.com/assets/images/logo-dark.svg',
+		shortDescription: 'designed and implemented a web application for receiving SMS messages, integrated PayPal and Stripe payment gateways to facilitate secure transactions, thereby enhancing the services provided to clients.'
 	},
 	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
+		slug: 'Full-stack-web-developer',
+		company: 'Smart-Influencer',
+		description: smart_influance,
+		contract: ContractType.Internship,
 		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
-		color: 'green',
+		location: 'On Site',
+		period: { from: new Date(2022, 3, 1), to: new Date(2022, 4, 5) },
+		skills: getSkills('css', 'html', 'js', 'MySQL', 'Laravel', "PHP"),
+		name: 'Full stack web developer',
+		color: 'cyan',
 		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
+		logo: 'https://smartinfluencer.ma/wp-content/uploads/2021/11/SI.gif',
+		shortDescription: 'Developed a web-based helpdesk application using Laravel v8 and JavaScrip.'
 	}
 ];
 
---
+++ src/lib/md/it-lab.md
@@ -0,0 +1,18 @@
+# Web-based helpdesk
+
+[`SMS App Link`](https://www.number4sms.com)
+<br/>
+[`Manga app`](https://github.com/mohamedelazzouzi1997/multipl-manga)
+<br/>
+[`Manga App Video Link`](https://drive.google.com/file/d/1ZLrGL52kW90oSFnOLEJ1lUtZP8j5U9q3/view?usp=sharing)
+<br/>
+
+---
+
+<br/>
+
+- Developed, maintained, and enhanced web applications and websites.
+- Built new websites based on client requests and requirements.
+- Assisted in the maintenance and improvement of client websites.
+- Collaborated with the Manager to optimize processes, organize front-end tasks, and handle additional responsibilities as required.
+- Successfully designed and implemented a web application for receiving SMS messages, integrated PayPal and Stripe payment gateways to facilitate secure transactions, thereby enhancing the services provided to clients.
