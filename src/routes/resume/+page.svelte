33648fe4d46623d8aa23412fcf3ffe367760878b
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2026-01-17 04:00:16 +0100

    fix cv download path for with svelte setting config

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { data, title } from '@data/resume';
-
+ import { base } from '$app/paths';
 	import Chip from '$lib/components/Chip/Chip.svelte';
 	import CommonPage from '$lib/components/CommonPage.svelte';
 </script>
@@ -8,7 +8,7 @@
 <CommonPage {title}>
 	<div class="resume">
 		{#if data}
-			<a href={data} download>
+			<a href={base}{data} download>
 				<Chip size={'1.25em'}>Download</Chip>
 			</a>
 		{:else}
